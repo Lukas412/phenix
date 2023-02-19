8c0fa67f0e32fb0c9d252cb5cac325ceac60ea53
lukas <lukas.weber.4.12@gmail.com>
2023-02-19 19:16:59 +0100

    implement sub

--- phenix_core/src/operations/sub.rs
+++ phenix_core/src/operations/sub.rs
@@ -1,4 +1,7 @@
 use std::fmt::Debug;
+use std::ops::Sub;
+use crate::{ComplexCreationArguments, Evaluate, Runtime};
+use crate::evaluate::EvaluateResult;
 
 pub trait EvaluateSub<Rhs = Self> {
   type Output;
@@ -7,7 +10,33 @@ pub trait EvaluateSub<Rhs = Self> {
 }
 
 #[derive(Clone, Debug)]
-pub struct SubOperation<T> {
+pub struct SubOperation<T, R> {
   expression: T,
-  rhs: T,
+  rhs_expression: R,
+}
+
+impl<T, R> SubOperation<T, R> {
+  pub fn new<A, B>(expression: A, rhs_expression: B) -> Self
+  where
+    A: Into<T>,
+    B: Into<R>,
+  {
+    Self {
+      expression: expression.into(),
+      rhs_expression: rhs_expression.into(),
+    }
+  }
+}
+
+impl<T, V, R> Evaluate<V> for SubOperation<T, R>
+where
+  T: Evaluate<V>,
+  R: Evaluate<V>,
+  V: Sub<V, Output = EvaluateResult<V>>,
+{
+  fn evaluate(&self, runtime: &Runtime, arguments: ComplexCreationArguments) -> EvaluateResult<V> {
+    let value = self.expression.evaluate(runtime, arguments.clone())?;
+    let rhs_value = self.rhs_expression.evaluate(runtime, arguments)?;
+    value + rhs_value
+  }
 }
