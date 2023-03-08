f27aeb451ecf648ff41edb7580341fd7de546cf6
lukas <lukas.weber.4.12@gmail.com>
2023-03-08 09:56:22 +0100

    implement evaluate for two value tuple

--- phenix_core/src/evaluate.rs
+++ phenix_core/src/evaluate.rs
@@ -3,7 +3,30 @@ use crate::{ComplexCreationArguments, EvaluateError, Runtime};
 pub trait Evaluate {
   type Result;
 
-  fn evaluate(&self, runtime: &Runtime, arguments: ComplexCreationArguments) -> EvaluateResult<Self::Result>;
+  fn evaluate(
+    &self,
+    runtime: &Runtime,
+    arguments: ComplexCreationArguments,
+  ) -> EvaluateResult<Self::Result>;
 }
 
 pub type EvaluateResult<V> = Result<V, EvaluateError>;
+
+impl<Expression, OtherExpression> Evaluate for (Expression, OtherExpression)
+where
+  Expression: Evaluate,
+  OtherExpression: Evaluate,
+{
+  type Result = (Expression::Result, OtherExpression::Result);
+
+  fn evaluate(
+    &self,
+    runtime: &Runtime,
+    arguments: ComplexCreationArguments,
+  ) -> EvaluateResult<Self::Result> {
+    let (expression, other_expression) = self;
+    let value = expression.evaluate(runtime, arguments.clone())?;
+    let other_value = other_expression.evaluate(runtime, arguments)?;
+    Ok((value, other_value))
+  }
+}
