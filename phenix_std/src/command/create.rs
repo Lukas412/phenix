25a6b3e3affe02b78df469fe8cae9202add9f7fd
lukas <lukas.weber.4.12@gmail.com>
2023-04-13 00:42:41 +0200

    wip

--- phenix_std/src/command/create.rs
+++ phenix_std/src/command/create.rs
@@ -1,7 +1,7 @@
 use phenix_core::{ActionValue, CommandValue, GetArgumentOperation};
 
 pub(crate) fn create_command_value() -> ActionValue {
-  let name = GetArgumentOperation::new("std:command$name").into();
-  let arguments = GetArgumentOperation::new("std:command$arguments").into();
+  let name = GetArgumentOperation::new("std:command$name");
+  let arguments = GetArgumentOperation::new("std:command$arguments");
   CommandValue::new(name, arguments).into()
 }
