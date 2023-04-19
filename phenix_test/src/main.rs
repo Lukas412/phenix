c5b56e147eed252f3dfb94d96eb22c09f42fc8e2
Lukas412 <lukas.weber.4.12@gmail.com>
2023-04-19 19:14:48 +0200

    wip

--- phenix_test/src/main.rs
+++ phenix_test/src/main.rs
@@ -1,9 +1,8 @@
 use phenix_core::{ActionValue, AsBash, ComplexCreationBuilder, RuntimeBuilder};
-use phenix_std::RuntimeBuilderStdExt;
 use phenix_svelte::{PhenixSvelteExtension, SVELTE_PROJECT, SVELTE_PROJECT__NAME};
 
 fn main() {
-  let runtime = RuntimeBuilder::default().with_std().with_svelte().build();
+  let runtime = RuntimeBuilder::default().with_svelte().build();
 
   let creation = ComplexCreationBuilder::new(SVELTE_PROJECT)
     .with(SVELTE_PROJECT__NAME, "my_test_project")
