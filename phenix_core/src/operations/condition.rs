cf6188087f357c183a07afa79b48a1492223b059
Lukas412 <lukas.weber.4.12@gmail.com>
2023-04-19 14:46:33 +0200

    wip

--- phenix_core/src/operations/condition.rs
+++ phenix_core/src/operations/condition.rs
@@ -1,8 +1,27 @@
-use crate::BooleanExpression;
+use crate::evaluate::EvaluateResult;
+use crate::{BooleanExpression, ComplexCreationArguments, Evaluate, Runtime};
 
-#[allow(unused)]
 pub struct ConditionOperation<Expression> {
   condition: BooleanExpression,
   then: Expression,
   other: Expression,
+}
+
+impl<Expression, Value> Evaluate for ConditionOperation<Expression>
+where
+  Expression: Evaluate<Result = Value>,
+{
+  type Result = Value;
+
+  fn evaluate(
+    &self,
+    runtime: &Runtime,
+    arguments: &ComplexCreationArguments,
+  ) -> EvaluateResult<Self::Result> {
+    if self.condition.evaluate(runtime, arguments)? {
+      self.then.evaluate(runtime, arguments)
+    } else {
+      self.other.evaluate(runtime, arguments)
+    }
+  }
 }
