10d43252b4ed1a669b36481571fa4b8359dc91dd
lukas <lukas.weber.4.12@gmail.com>
2023-04-24 00:02:55 +0200

    wip

--- extensions/phenix_svelte/src/lib.rs
+++ extensions/phenix_svelte/src/lib.rs
@@ -1,6 +1,9 @@
 pub use {
   ext::PhenixSvelteExtension,
-  phenix_identifiers::{SVELTE_PROJECT, SVELTE_PROJECT_INIT, SVELTE_PROJECT__NAME},
+  phenix_identifiers::{
+    SVELTE_PAGE, SVELTE_PAGE_CONTENT, SVELTE_PAGE__SCRIPT, SVELTE_PROJECT, SVELTE_PROJECT_INIT,
+    SVELTE_PROJECT__NAME,
+  },
   project::{new_svelte_project_init_operation, new_svelte_project_operation},
 };
 
